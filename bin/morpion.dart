76afdc04b8e98394818814f6995cd44cc01b598f
Richard <richardanagonou0@gmail.com>
2025-11-25 17:09:22 +0100

    Début: Morpion en Dart

---
+++ bin/morpion.dart
@@ -0,0 +1,5 @@
+import 'package:morpion/morpion.dart' as morpion;
+
+void main(List<String> arguments) {
+  print('Hello world: ${morpion.calculate()}!');
+}
---
+++ test/morpion_test.dart
@@ -0,0 +1,8 @@
+import 'package:morpion/morpion.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
